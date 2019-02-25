6ccbbe43c7309efd19c210fd35dfbc5211c723d7
Eeo Jun <141bytes@gmail.com>
2019-02-25 22:55:39 +0000

    a s s e r t

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 int main() {
 	array a;
 	a = array_new();
-	array_resize(&a, 10);
+	assert(array_resize(&a, 10));
 
 	int v;
 
@@ -14,15 +14,15 @@ int main() {
 		assert(v == 0);
 	}
 
-	array_resize(&a, 5);
+	assert(array_resize(&a, 5));
 	assert(!array_get(&a, 6, &v));
 
-	array_resize(&a, 10);
+	assert(array_resize(&a, 10));
 	assert(array_get(&a, 6, &v));
 	assert(v == 0);
 
 	for (int i = 0; i < 10; i++) {
-		array_set(&a, i, i);
+		assert(array_set(&a, i, i));
 		assert(array_get(&a, i, &v));
 		assert(v == i);
 	}
