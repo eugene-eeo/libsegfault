782729c594cbc336f1d84c98ea8d052307d748db
Eeo Jun <141bytes@gmail.com>
2020-09-02 12:48:51 +0100

    update

--- array.c
+++ array.c
@@ -1,9 +1,10 @@
 #include <stdlib.h>
+#include <stddef.h>
 
 
 typedef struct {
-	unsigned long size;
-	int *N;
+    size_t size;
+    int *N;
 } array;
 
 
@@ -11,60 +12,60 @@ typedef struct {
  * Returns a new array
  */
 array array_new() {
-	array x = {0, NULL};
-	return x;
+    array x = {0, NULL};
+    return x;
 }
 
 /*
  * Free array.
  */
 void array_free(array *a) {
-	free(a->N);
-	a->size = 0;
-	a->N = NULL;
+    free(a->N);
+    a->size = 0;
+    a->N = NULL;
 }
 
 /*
  * Resizes array to size new_size.
  */
-int array_resize(array *a, unsigned long new_size) {
-	if (a->N == NULL) {
-		a->N = calloc(new_size, sizeof(int));
-		if (a->N == NULL)
-			return 0;
-	} else {
-		// realloc and fill memory with zeroes
-		int *ptr = realloc(a->N, new_size * sizeof(int));
-		if (ptr == NULL)
-			return 0;
-		a->N = ptr;
-		if (new_size > a->size)
-			for (unsigned long i = 0; i < new_size - a->size; i++)
-				ptr[a->size + i] = 0;
-	}
-	a->size = new_size;
-	return 1;
+int array_resize(array *a, size_t new_size) {
+    if (a->N == NULL) {
+        a->N = calloc(new_size, sizeof(int));
+        if (a->N == NULL)
+            return 0;
+    } else {
+        // realloc and fill memory with zeroes
+        int *ptr = realloc(a->N, new_size * sizeof(int));
+        if (ptr == NULL)
+            return 0;
+        a->N = ptr;
+        if (new_size > a->size)
+            for (unsigned long i = 0; i < new_size - a->size; i++)
+                ptr[a->size + i] = 0;
+    }
+    a->size = new_size;
+    return 1;
 }
 
 /*
  * Sets a[idx] = val and return 1 if possible, resizing if
  * neccessary. Return 0 => resize failed.
  */
-int array_set(array *a, int idx, int val) {
-	while ((unsigned long) idx >= a->size)
-		if (!array_resize(a, a->size*2))
-			return 0;
-	a->N[idx] = val;
-	return 1;
+int array_set(array *a, size_t idx, int val) {
+    while (idx >= a->size)
+        if (!array_resize(a, a->size*2))
+            return 0;
+    a->N[idx] = val;
+    return 1;
 }
 
 /*
  * Sets *dst = a[idx] and return 1 if possible, otherwise do
  * nothing to dst and return 0.
  */
-int array_get(array *a, int idx, int *dst) {
-	if ((unsigned long) idx >= a->size)
-		return 0;
-	*dst = a->N[idx];
-	return 1;
+int array_get(array *a, size_t idx, int *dst) {
+    if (idx >= a->size)
+        return 0;
+    *dst = a->N[idx];
+    return 1;
 }
--- main.c
+++ main.c
@@ -7,128 +7,144 @@
 #include <string.h>
 
 void test_rc4() {
-	char *k = "Key";
-	char *m = "Plaintext";
-	char *c = malloc(strlen(m) + 1);
-	char *b = malloc(strlen(m) + 1);
+    char *k = "Key";
+    char *m = "Plaintext";
+    char *c = malloc(strlen(m) + 1);
+    char *b = malloc(strlen(m) + 1);
 
-	assert(c != NULL);
-	assert(b != NULL);
+    assert(c != NULL);
+    assert(b != NULL);
 
-	rc4_encrypt(k, m, c);
-	rc4_decrypt(k, c, b);
+    rc4_encrypt(k, m, c);
+    rc4_decrypt(k, c, b);
 
-	assert(c[0] == '\xBB');
-	assert(c[1] == '\xF3');
-	assert(c[2] == '\x16');
-	assert(c[3] == '\xE8');
-	assert(c[4] == '\xD9');
-	assert(c[5] == '\x40');
-	assert(c[6] == '\xAF');
-	assert(c[7] == '\x0A');
-	assert(c[8] == '\xD3');
+    assert(c[0] == '\xBB');
+    assert(c[1] == '\xF3');
+    assert(c[2] == '\x16');
+    assert(c[3] == '\xE8');
+    assert(c[4] == '\xD9');
+    assert(c[5] == '\x40');
+    assert(c[6] == '\xAF');
+    assert(c[7] == '\x0A');
+    assert(c[8] == '\xD3');
 
-	assert(strlen(c) == strlen(m));
-	assert(strcmp(b, m) == 0);
+    assert(strlen(c) == strlen(m));
+    assert(strcmp(b, m) == 0);
 }
 
 void test_dset() {
-	dset *a = dset_new();
-	dset *b = dset_new();
-	dset *c = dset_new();
-	dset *d = dset_new();
+    dset *a = dset_new();
+    dset *b = dset_new();
+    dset *c = dset_new();
+    dset *d = dset_new();
 
-	assert(dset_find(a) == a);
-	assert(dset_find(b) == b);
-	assert(dset_find(c) == c);
-	assert(dset_find(d) == d);
+    assert(dset_find(a) == a);
+    assert(dset_find(b) == b);
+    assert(dset_find(c) == c);
+    assert(dset_find(d) == d);
 
-	dset_union(a, b);
-	dset_union(c, d);
+    dset_union(a, b);
+    dset_union(c, d);
 
-	assert(dset_find(a) == dset_find(b));
-	assert(dset_find(c) == dset_find(d));
-	assert(dset_find(a) != dset_find(c));
+    assert(dset_find(a) == dset_find(b));
+    assert(dset_find(c) == dset_find(d));
+    assert(dset_find(a) != dset_find(c));
 
-	dset_union(a, c);
-	assert(dset_find(a) == dset_find(b));
-	assert(dset_find(a) == dset_find(c));
-	assert(dset_find(a) == dset_find(d));
+    dset_union(a, c);
+    assert(dset_find(a) == dset_find(b));
+    assert(dset_find(a) == dset_find(c));
+    assert(dset_find(a) == dset_find(d));
 
-	free(a);
-	free(b);
-	free(c);
-	free(d);
+    free(a);
+    free(b);
+    free(c);
+    free(d);
 }
 
 void test_hashmap() {
-	hashmap h = hashmap_new();
-	for (int i = 0; i < 50; i++) {
-		assert(hashmap_put(&h, i, i));
-	}
+    int ok = 0;
+    hashmap h = hashmap_new();
+    for (int i = 0; i < 50; i++) {
+        assert(hashmap_put(&h, i, i));
+    }
 
-	for (int i = 0; i < 50; i++) {
-		int v;
-		assert(hashmap_get(&h, i, &v));
-		assert(v == i);
-	}
+    for (int i = 0; i < 50; i++) {
+        int v;
+        ok = hashmap_get(&h, i, &v);
+        assert(ok);
+        assert(v == i);
+    }
 
-	for (int i = 0; i < 25; i++) {
-		assert(hashmap_delete(&h, 2*i));
-	}
+    for (int i = 0; i < 25; i++) {
+        ok = hashmap_delete(&h, 2*i);
+        assert(ok);
+    }
 
-	for (int i = 0; i < 25; i++) {
-		int v;
-		// check that we cannot get a deleted item
-		assert(!hashmap_get(&h, 2*i, &v));
-		assert(hashmap_get(&h, 2*i+1, &v));
-		assert(v == 2*i+1);
-	}
+    for (int i = 0; i < 25; i++) {
+        int v;
+        // check that we cannot get a deleted item
+        ok = hashmap_get(&h, 2*i, &v);
+        assert(!ok);
+        ok = hashmap_get(&h, 2*i+1, &v);
+        assert(ok);
+        assert(v == 2*i+1);
+    }
 
-	cursor c = cursor_create(&h);
-	entry *e;
-	while ((e = cursor_next(&c)) != NULL)
-		assert(e->k % 2 == 1);
+    cursor c = cursor_create(&h);
+    entry *e;
+    while ((e = cursor_next(&c)) != NULL)
+        assert(e->k % 2 == 1);
 
-	hashmap_free(&h);
-	assert(cursor_next(&c) == NULL);
+    hashmap_free(&h);
+    assert(cursor_next(&c) == NULL);
 }
 
 void test_array() {
-	array a;
-	a = array_new();
-	assert(array_resize(&a, 10));
+    int ok = 0;
+    array a;
+    a = array_new();
+    ok = array_resize(&a, 10);
+    assert(ok);
 
-	int v;
+    int v;
 
-	for (int i = 0; i < 10; i++) {
-		assert(array_get(&a, i, &v));
-		assert(v == 0);
-	}
+    for (int i = 0; i < 10; i++) {
+        ok = array_get(&a, i, &v);
+        assert(ok);
+        assert(v == 0);
+    }
 
-	assert(array_resize(&a, 5));
-	assert(!array_get(&a, 6, &v));
+    ok = array_resize(&a, 5);
+    assert(ok);
+    ok = array_get(&a, 6, &v);
+    assert(!ok);
 
-	assert(array_resize(&a, 10));
-	assert(array_get(&a, 6, &v));
-	assert(v == 0);
+    ok = array_resize(&a, 10);
+    assert(ok);
+    ok = array_get(&a, 6, &v);
+    assert(ok);
+    assert(v == 0);
 
-	for (int i = 0; i < 10; i++) {
-		assert(array_set(&a, i, i));
-		assert(array_get(&a, i, &v));
-		assert(v == i);
-	}
+    for (int i = 0; i < 10; i++) {
+        ok = array_set(&a, i, i);
+        assert(ok);
+        ok = array_get(&a, i, &v);
+        assert(ok);
+        assert(v == i);
+    }
 
-	assert(array_set(&a, 100, 69));
-	assert(array_get(&a, 100, &v));
-	assert(v == 69);
+    ok = array_set(&a, 100, 69);
+    assert(ok);
+    ok = array_get(&a, 100, &v);
+    assert(ok);
+    assert(v == 69);
 
-	array_free(&a);
+    array_free(&a);
 }
 
 int main() {
-	test_array();
-	test_hashmap();
-	test_dset();
-	test_rc4();
+    test_array();
+    test_hashmap();
+    test_dset();
+    test_rc4();
 }
